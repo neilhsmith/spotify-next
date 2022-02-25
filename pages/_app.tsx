dd1263fa82f7c68ef55e727d2bc3f5833213d270
Neil Smith <me@neilsmith.fyi>
2022-02-25 01:01:48 -0600

    chore(setup): add Chakra UI Setup Chakra UI and setup a dark theme for customization.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { ChakraProvider } from "@chakra-ui/react";
+import darkTheme from "themes/dark";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ChakraProvider theme={darkTheme} resetCSS>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </SessionProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import type { NextPage } from "next";
 import { signIn } from "next-auth/react";
+import { Button, useColorMode } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <button onClick={() => signIn("spotify", { callbackUrl: "/app" })}>
-      Sign In with Spotify
-    </button>
+    <>
+      <Button onClick={() => signIn("spotify", { callbackUrl: "/app" })}>
+        Sign In with Spotify
+      </Button>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
+    </>
   );
 };
 
